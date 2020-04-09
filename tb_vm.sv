3f8eabb2aa14bf386c15f75a0b4eaf12d8413605
navahc <54077484+navahc@users.noreply.github.com>
2020-04-08 19:35:54 -0700

    Update tb_vm.sv

--- tb_vm.sv
+++ tb_vm.sv
@@ -1,8 +1,14 @@
-module tb_vm;
+module tb_vm #(//VALUES TO FEED
+parameter ITEM_SUPP,	//WHICH ITEM TO RESTOCK	(1 To 6 for BUTTONS 0 THROUGH 6; 0, 7, 8 INDICATE NO ITEM)
+		  COUNT_SUPP,	//FOR THE ITEM TO RESTOCK HOW MANY ITEMS TO RESTOCK (MAXIMUM VALUE IS 15) 
+	      COST_SUPP,	//FOR THE ITEM TO RESTOCK HOW MUCH SHOULD BE THE COST FOR THAT ITEM (MAXIMUM VALUE IS 255) 
+	      COINS,		//COINS THE USER WILL INPUT (1-NICKEL; 2-DIME; 3-QUARTER; 0-NO COINS)
+  BUTTON);		// BUTTON THE USER WILL PRESS TO FETCH AN ITEM (VALID INPUTS ARE 0 To 5);
+		  
 
 //SUPPLIER INPUTS TO DESIGN
 bit valid_s;
-bit [2:0] item_s;
+bit [2:0] items_s;
 bit [3:0] count_s;
 bit [7:0] cost_s;
 bit enter_key;
@@ -16,13 +22,9 @@ bit clk, rst;
 
 //MISC. INPUTS
 bit load_flag;
+int i;
 
-//VALUES TO FEED
-parameter ITEM_SUPP,	//WHICH ITEM TO RESTOCK	(1 To 6 for BUTTONS 0 THROUGH 6; 0, 7, 8 INDICATE NO ITEM)
-		  COUNT_SUPP,	//FOR THE ITEM TO RESTOCK HOW MANY ITEMS TO RESTOCK (MAXIMUM VALUE IS 15) 
-	      COST_SUPP,	//FOR THE ITEM TO RESTOCK HOW MUCH SHOULD BE THE COST FOR THAT ITEM (MAXIMUM VALUE IS 255) 
-	      COINS,		//COINS THE USER WILL INPUT (1-NICKEL; 2-DIME; 3-QUARTER; 0-NO COINS)
-		  BUTTON;		// BUTTON THE USER WILL PRESS TO FETCH AN ITEM (VALID INPUTS ARE 0 To 5)
+
 
 //MODULE INSTANTIATION
 vm dut(.valid_s(valid_s),
@@ -31,8 +33,8 @@ vm dut(.valid_s(valid_s),
 	   .cost_s(cost_s),
 	   .enter_key(enter_key),
 	   .clk(clk),
-	   .rst(rst)
-	   .coins(coins);)
+       .rst(rst),
+       .coins(coins));
 	   
 initial 
 begin
@@ -57,12 +59,15 @@ begin
 				valid_s=0;
 				rst=0;
 				break;
+				//$display(values)
 			end
 		end
+    end
 		
 	if ((rst==0) && (valid_s==0))
 	begin
 		coins=COINS;
+		//for()
 		enter_key=1;
 		button[BUTTON]=1;
 	end
